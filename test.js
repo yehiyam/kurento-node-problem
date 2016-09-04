b6d9e4e787ef002d8f2159f61bd2a735c3bfc3f2
yehiyam <yehiyam@gmail.com>
2016-09-04 11:12:13 +0300

    add test

--- test.js
+++ test.js
@@ -1,13 +1,16 @@
 'use strict'
 var kurento = require('kurento-client');
+var chai = require('chai');
+var expect = chai.expect;
+
 var ws_uri= 'ws://localhost:8888/kurento';
 
 function getClient() {
     return new Promise((resolve, reject) => {
         kurento(ws_uri, function (error, _kurentoClient) {
             if (error) {
-                console.log("Could not find media server at address " + argv.ws_uri);
-                return reject("Could not find media server at address" + argv.ws_uri
+                console.log("Could not find media server at address " + ws_uri);
+                return reject("Could not find media server at address" + ws_uri
                     + ". Exiting with error " + error);
             }
             return resolve(_kurentoClient);
@@ -17,11 +20,16 @@ function getClient() {
 }
 
 
-console.log('Trying to connect');
-getClient().then(kurentoClient => {
-    console.log('Connected')
-}).catch(error => {
-    console.log(error);
+describe('try to connect',()=>{
+    it('should return from the promise',done=>{
+        getClient().then(kurentoClient => {
+            console.log('Connected')
+            done();
+        }).catch(error => {
+            console.log(error);
+        })
+    })
 })
+
 
 
